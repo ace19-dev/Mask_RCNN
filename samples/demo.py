a46e7a315c196c44a95f7ba824ac2ce1ed1561d2
ace19-dev <acemc19@gmail.com>
2018-05-11 14:44:53 +0900

    update show_mask config

--- samples/demo.py
+++ samples/demo.py
@@ -81,5 +81,5 @@ results = model.detect([image], verbose=1)
 # Visualize results
 r = results[0]
 visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                            class_names, r['scores'])
+                            class_names, r['scores'], show_mask=False)
 
